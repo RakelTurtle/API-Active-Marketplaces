a1a0bde795a0fbac6e1ca4b50844aa5106d389f9
RakelTurtle <developer.02@turtlebp.com>
2022-10-17 19:54:50 -0300

    creating marketplace views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import MarketplaceSerializer
+
+from .models import Marketplace
+
+
+class MarketplaceAPIView(APIView):
+    """
+    API Marketplaces Turtle Brand Protection
+    """
+
+    def get(self, request):
+        api = Marketplace.objects.all()
+        serializer = MarketplaceSerializer(api, many=True)
+        return Response(serializer.data)
+
+        
+
