ef229beee0a4732c1c9c147e476672602e431288
RakelTurtle <developer.02@turtlebp.com>
2022-10-17 19:14:34 -0300

    creating marketplace models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Marketplace(models.Model):
+    name = models.CharField(max_length=100)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Marketplace"
+        verbose_name_plural = "Marketplaces"
+
+    def __str__(self):
+        return self.name
